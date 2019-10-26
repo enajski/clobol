ab0afd39874c1b3435455fd3218db12597ef6fbb
Wojtek Franke <enajski@gmail.com>
2019-10-26 22:05:04 +0200

    Add gnu-cobol hello world

---
+++ hello_world.cob
@@ -0,0 +1,7 @@
+     *> GnuCOBOL Hello World example
+      identification division.
+      program-id. hello.
+      procedure division.
+      display    "Hello, world!"
+      end-display.
+      goback.
